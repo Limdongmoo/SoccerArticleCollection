c06978dd72e50acc349b30ec6b9f8bac6e23176b
Limjongho <whdgh9873@icloud.com>
2022-10-05 21:30:07 +0900

    RestController -> Controller  변경, HealthCheckController(서버 정상 작동 여부 확인 컨트롤러) 생성

--- src/main/java/com/example/SoccerArticleCollection/controller/MatchController.java
+++ src/main/java/com/example/SoccerArticleCollection/controller/MatchController.java
@@ -3,13 +3,13 @@ package com.example.SoccerArticleCollection.controller;
 import com.example.SoccerArticleCollection.domain.match.Match;
 import com.example.SoccerArticleCollection.service.MatchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/match")
 public class MatchController {
--- src/main/java/com/example/SoccerArticleCollection/controller/PlayerController.java
+++ src/main/java/com/example/SoccerArticleCollection/controller/PlayerController.java
@@ -3,13 +3,13 @@ package com.example.SoccerArticleCollection.controller;
 import com.example.SoccerArticleCollection.domain.player.TopPlayer;
 import com.example.SoccerArticleCollection.service.PlayerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/player")
 @RequiredArgsConstructor
 public class PlayerController {
